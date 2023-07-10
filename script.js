7762df8606dada96eeff015d14fc6fd19866e3c1
David Forbes <davidforbesi@hotmail.com>
2023-07-10 17:27:20 +0100

    added in input and functions to request weather from different locations

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const iconData = document.querySelector('img');
-const locationData = document.querySelector('#location');
-const conditionData = document.querySelector('#condition');
 const body = document.querySelector('body');
 const wrapper = document.querySelector('#content-wrapper');
-
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
 navigator.geolocation.getCurrentPosition((position) => {
-  getWeather(position.coords.latitude, position.coords.longitude);
+  getWeather(`${position.coords.latitude}, ${position.coords.longitude}`);
 });
 
-async function getWeather(latitude, longitude) {
+button.addEventListener('click', () => {
+  getWeather(input.value);
+})
+
+async function getWeather(location) {
   try {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=eb5dbdb018c94d67812132539230707&q=${latitude},${longitude}&aqi=no`);
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=eb5dbdb018c94d67812132539230707&q=${location}&aqi=no`);
     const json = await response.json();
-    const tzID = json.location.tz_id;
-    const cityArray = tzID.split('/');
-    const city = cityArray[1];
-    await getBackgroundImage(city);
+    const weather = json.current.condition.text;
+    await getBackgroundImage(weather);
     displayWeather(json);
   } catch (err) {
     console.log(err);
@@ -24,9 +24,16 @@ async function getWeather(latitude, longitude) {
 }
 
 function displayWeather(data) {
-  iconData.src = data.current.condition.icon;
+  wrapper.innerHTML = '';
+  const locationData = document.createElement('h1');
+  const conditionData = document.createElement('h2');
+  const iconData = document.createElement('img');
   locationData.textContent = data.location.name;
   conditionData.textContent = data.current.condition.text;
+  iconData.src = data.current.condition.icon;
+  wrapper.appendChild(locationData);
+  wrapper.appendChild(conditionData);
+  wrapper.appendChild(iconData);
   let array = Object.entries(data.current);
   let filteredArray = [array[2], array[6], array[8], array[9], array[10], array[12], array[14], array[15], array[16], array[19], array[20], array[21]];
   filteredArray.forEach(item => {
@@ -40,9 +47,8 @@ function getBackgroundImage(cityValue) {
   fetch(`https://api.unsplash.com/search/photos?query=${cityValue}&client_id=WdKmaCWM493X2x7wxCB_uTQC6B_rWj7tkWlewDTIUQ4`)
     .then(response => response.json())
     .then(jsonData => {
-      console.log(jsonData);
       const num = Math.floor(Math.random() * 10) + 1;
-      body.style.cssText = `background: URL('${jsonData.results[num].urls.regular}'); background-size: cover;
-    background - repeat: no - repeat; background-postion:center;`;
-    })
+      body.style.cssText = `background: URL('${jsonData.results[num].urls.full}'); background-size: cover;
+    background - repeat: no - repeat;`;
+    });
 }
