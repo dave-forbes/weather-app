c4ff1fda2f774a705abffa37138c487ae21b3334
David Forbes <davidforbesi@hotmail.com>
2023-07-10 10:22:37 +0100

    created function to display icon

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const icon = document.querySelector('img');
+
 async function getWeather() {
   try {
     const response = await fetch("https://api.weatherapi.com/v1/current.json?key=eb5dbdb018c94d67812132539230707&q=London&aqi=no");
-    console.log(response);
     const json = await response.json();
-    console.log(json);
+    displayWeather(json);
   } catch (err) {
     console.log(err);
   }
+}
+
+function displayWeather(data) {
+  icon.src = data.current.condition.icon;
 }
 
 getWeather();
