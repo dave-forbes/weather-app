566433d4c930e60228cf27234460ec0175bada3e
David Forbes <davidforbesi@hotmail.com>
2023-10-02 13:50:53 +0100

    modified display weather to correspond with new more info section, added event listener to show/hide more info, changed navigator geolocation code into async function, reorganised code with all event listeners grouped together to become more readable

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import date from "date-and-time";
 import wcc from "world-countries-capitals";
 
-const input = document.querySelector("input");
-const button = document.querySelector("i.fa-magnifying-glass");
-
-navigator.geolocation.getCurrentPosition((position) => {
-  getWeather(`${position.coords.latitude}, ${position.coords.longitude}`);
-});
-
-button.addEventListener("click", () => {
-  getWeather(input.value);
-});
-
-const shuffleButton = document.querySelector("i.fa-shuffle");
-
-shuffleButton.addEventListener("click", getWeatherForRandomLocation);
-
 async function getWeather(location) {
   try {
     const response = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=eb5dbdb018c94d67812132539230707&q=${location}&aqi=no`
     );
     const json = await response.json();
-    console.log(json);
     displayWeather(json);
   } catch (err) {
     console.log(err);
@@ -51,61 +35,119 @@ function displayWeather(data) {
   const temp = document.querySelector(".temperature");
   temp.textContent = `${data.current.temp_c}°`;
 
-  const body = document.querySelector("body");
+  const mainContent = document.querySelector(".main-content");
 
   if (data.current.is_day == 1) {
-    body.classList.add("day-time-background");
-    body.classList.remove("night-time-background");
+    mainContent.classList.add("day-time-background");
+    mainContent.classList.remove("night-time-background");
   } else {
-    body.classList.add("night-time-background");
-    body.classList.remove("day-time-background");
+    mainContent.classList.add("night-time-background");
+    mainContent.classList.remove("day-time-background");
   }
 
-  // wrapper.innerHTML = "";
+  const country = document.querySelector(".country");
 
-  // const locationData = document.createElement("h1");
-  // locationData.textContent = data.location.name;
-  // wrapper.appendChild(locationData);
+  let icon;
+  if (data.location.tz_id.includes("Europe")) {
+    icon = '<i class="fa-solid fa-earth-europe"></i>';
+  } else if (data.location.tz_id.includes("Asia")) {
+    icon = '<i class="fa-solid fa-earth-asia"></i>';
+  } else if (data.location.tz_id.includes("Africa")) {
+    icon = '<i class="fa-solid fa-earth-africa"></i>';
+  } else if (data.location.tz_id.includes("America")) {
+    icon = '<i class="fa-solid fa-earth-americas"></i>';
+  } else if (data.location.tz_id.includes("Pacific")) {
+    icon = '<i class="fa-solid fa-earth-oceania"></i>';
+  } else {
+    icon = '<i class="fa-solid fa-earth-americas"></i>';
+  }
+  country.innerHTML = `
+    ${icon}
+    <p class="translucent">Country</p>
+    <p>${data.location.country}</p>
+  `;
 
-  // const conditionData = document.createElement("h2");
-  // conditionData.textContent = data.current.condition.text;
-  // wrapper.appendChild(conditionData);
+  const tempData = document.querySelector(".temp");
+  tempData.innerHTML = `
+    <i class="fa-solid fa-temperature-quarter"></i> 
+    <p class="translucent">Temperature</p>
+    <p>${data.current.temp_c}°</p>
+  `;
 
-  // const iconData = document.createElement("img");
-  // iconData.src = data.current.condition.icon;
-  // wrapper.appendChild(iconData);
+  const wind = document.querySelector(".wind");
+  wind.innerHTML = `
+    <i class="fa-solid fa-wind"></i>
+    <p class="translucent">Wind</p>
+    <p>${data.current.wind_mph}mph ${data.current.wind_dir}</p>
+  `;
 
-  // const temp = document.createElement("p");
-  // temp.textContent = `${data.current.temp_c}°C`;
-  // wrapper.appendChild(temp);
+  const humidity = document.querySelector(".humidity");
+  humidity.innerHTML = `
+    <i class="fa-solid fa-droplet"></i>
+    <p class="translucent">Humidity</p>
+    <p>${data.current.humidity}%</p>
+  `;
 
-  // const wind = document.createElement("p");
-  // wind.textContent = `Wind Speed: ${data.current.wind_mph}mph`;
-  // wrapper.appendChild(wind);
+  const precipitation = document.querySelector(".precipitation");
+  precipitation.innerHTML = `
+    <i class="fa-solid fa-cloud-rain"></i>
+    <p class="translucent">Precipitation</p>
+    <p>${data.current.precip_mm}mm</p>
+  `;
 
-  // const windDirection = document.createElement("p");
-  // windDirection.textContent = `Wind Direction: ${data.current.wind_dir}`;
-  // wrapper.appendChild(windDirection);
+  const uv = document.querySelector(".uv");
+  uv.innerHTML = `
+    <i class="fa-regular fa-sun"></i>
+    <p class="translucent">UV Index</p>
+    <p>${data.current.uv}</p>
+  `;
+}
 
-  // const rain = document.createElement("p");
-  // rain.textContent = `Precipitation: ${data.current.precip_mm}mm`;
-  // wrapper.appendChild(rain);
-
-  // const humidity = document.createElement("p");
-  // humidity.textContent = `Humidity: ${data.current.humidity}%`;
-  // wrapper.appendChild(humidity);
+function showMoreData() {
+  const dataWrapper = document.querySelector(".data-wrapper");
+  const moreInfo = document.querySelector(".more-info");
+  dataWrapper.classList.toggle("hide");
+  moreInfo.classList.toggle("hide");
 }
 
 function getWeatherForRandomLocation() {
   const randomCountry = wcc.getRandomCountry();
   const randomCountryInfo = wcc.getCountryDetailsByName(randomCountry);
   const randomCapital = randomCountryInfo[0].capital;
-
   getWeather(randomCapital);
 }
 
-const menuButton = document.querySelector("i.fa-bars");
+async function getPosition() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(position),
+      (error) => reject(error)
+    );
+  });
+}
 
-menuButton.addEventListener("click", showMoreData);
+async function getWeatherBasedOnLocation() {
+  try {
+    const position = await getPosition();
+    const coordinates = `${position.coords.latitude}, ${position.coords.longitude}`;
+    getWeather(coordinates);
+  } catch (error) {
+    console.error("Error getting location:", error.message);
+  }
+}
 
-function showMoreData() {}
+function getWeatherFromSearch() {
+  const input = document.querySelector("input");
+  getWeather(input.value);
+}
+
+(function eventListeners() {
+  const menuButton = document.querySelector(".fa-circle-info");
+  const button = document.querySelector("i.fa-magnifying-glass");
+  const shuffleButton = document.querySelector("i.fa-shuffle");
+  menuButton.addEventListener("click", showMoreData);
+  button.addEventListener("click", getWeatherFromSearch);
+  shuffleButton.addEventListener("click", getWeatherForRandomLocation);
+})();
+
+getWeatherBasedOnLocation();
